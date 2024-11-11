4d391ccbc6a3ac4bc16462b13decaf080e4d9f3e
maamaan202 <2025maanka2025@gmail.com>
2024-11-11 19:28:20 +0300

    Delete src directory
    Jjwjw

--- src/EmptyCollectionException.java
+++
@@ -1,5 +0,0 @@
-public class EmptyCollectionException extends Exception {
-    public EmptyCollectionException(String collectionName) {
-        super("The " + collectionName + " is empty.");
-    }
-}
